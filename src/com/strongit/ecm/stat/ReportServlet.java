3bd7c9449fb431109e12c834dfc17fc45f59c90b
Jingguo Yao <yaojingguo@gmail.com>
2012-11-18 16:29:04 +0800

    Support xls download

---
+++ src/com/strongit/ecm/stat/ReportServlet.java
@@ -0,0 +1,27 @@
+package com.strongit.ecm.stat;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class ReportServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    doPost(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    OutputStream out = resp.getOutputStream();
+    Workbook wb = Stat.buildExcel("2011-01", "2013-01");
+    wb.write(out);
+  }
+}
